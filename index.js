78beb2c7af1994265eda7a39e46e48591815c732
Sheila O'Donnell <sheila@contexttravel.com>
2018-08-11 19:04:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function distanceFromHqInBlocks(blockNumber) {
-  if blockNumber > 42
+  if (blockNumber > 42) {
   result = blockNumber - 42
+  }
   
-  else if blockNumber < 42
+  else if (blockNumber < 42) {
   result = 42 - blockNumber
-  
+  }
+  result
 }
